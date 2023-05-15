61149550bc930d82711125d6c6fa6a8e8b4bb1cb
Areeb Adnan Khan <ak06865@st.habib.edu.pk>
2023-05-15 14:16:35 +0500

    a3

--- Lab7-8/working_08/findOptSolution.m
+++
@@ -1,8 +0,0 @@
-function finalsol = findOptSolution(placement)
-    x = placement(1); 
-    y = placement(2);
-    z = placement(3); 
-    phi = placement(4); 
-    solutions = fJointAngles(x, y, z, phi)
-    finalsol = real(optimial_org(solutions)); 
-end
